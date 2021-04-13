82c78abe4d4a7553f33462f22e0b6b62a6d5dc7b
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-04-13 13:17:07 -0700

    Question 8 complete

--- jsPrograms/programs.js
+++ jsPrograms/programs.js
@@ -97,3 +97,31 @@
 
 
 
+
+
+
+// 8.	 Write a JavaScript program to delete the rollno property from the following object. 
+//       Also print the object before or after deleting the property.
+// Sample object:
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
+
+
+
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12
+// };
+
+
+// console.log("Object before deleting rollno property", student)
+
+// delete student.rollno;
+
+// console.log("Object after deleting rollno property", student)
+
+
+
