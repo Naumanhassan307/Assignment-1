3f7fd449c5541a2b8e579db134efbb0cfd849e80
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-04-13 12:15:14 -0700

    Question 5 is complete

--- jsPrograms/programs.js
+++ jsPrograms/programs.js
@@ -9,6 +9,8 @@
 // console.log("input value in uppercase=>", uppercaseValue );
 
 
+
+
 // 2.	Write a js program to convert uppercase string to lowercase.
 
 // let inputValue = prompt("Enter Uppercase string", "HELLO WORLD")
@@ -17,3 +19,45 @@
 
 // let lowercaseValue = inputValue.toLowerCase()
 // console.log("input value in lowercase=>", lowercaseValue);
+
+
+
+
+// 3.	Write a js program to toggle case of each character of a string.
+
+// let inputString = "hello world! this is Js program"
+
+// let splitString = inputString.split(" ")
+// console.log(splitString)
+
+// console.log(splitString[0].charAt(0).toUpperCase());
+// console.log(splitString[0])
+
+
+// 5.	Write a js program to count total number of vowels and consonants in a string.
+
+
+// let vowels = 0
+// let consonants = 0
+
+// let str = "This is a Js Program"
+// let lowerStr = str.toLowerCase()
+// console.log(typeof(lowerStr))
+// console.log(lowerStr)
+// for(let i=0; i<lowerStr.length; i++){
+
+//     if((lowerStr.charAt(i) === "a" || lowerStr.charAt(i) === "e" ||lowerStr.charAt(i) === "i" ||lowerStr.charAt(i) === "o" ||lowerStr.charAt(i) === "u" ) && (lowerStr.charAt(i) !== " ") ) {
+//         vowels++
+//     }else if ((lowerStr.charAt(i) !== "a" || lowerStr.charAt(i) !== "e" ||lowerStr.charAt(i) !== "i" ||lowerStr.charAt(i) !== "o" ||lowerStr.charAt(i) !== "u" ) && (lowerStr.charAt(i) !== " ")){
+
+//         consonants++
+//     }
+    
+// }
+// console.log("Vowels are in this string =>", vowels)
+
+// console.log("consonants are in this string =>", consonants);
+
+
+
+
