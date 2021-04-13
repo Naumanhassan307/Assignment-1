50a57d6f99965d26392d1b87029225e32368e492
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-04-13 10:18:06 -0700

    question 1 complete

---
+++ jsPrograms/programs.js
@@ -0,0 +1,9 @@
+// 1.	Write a js program to convert lowercase string to uppercase.
+
+
+let inputValue = prompt("Enter lowercase string", "hello world")
+
+console.log("input value before uppercase=>",inputValue );
+
+let uppercaseValue = inputValue.toUpperCase()
+console.log("input value in uppercase=>", uppercaseValue );
