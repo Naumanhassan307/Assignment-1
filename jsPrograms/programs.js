7c5114734a103115ba9f4bbbe173c2972b50226c
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-04-13 17:58:01 -0700

    Question 22 complete

--- jsPrograms/programs.js
+++ jsPrograms/programs.js
@@ -294,3 +294,16 @@
 // }else{
 //     console.log("Number is invalid")
 // }
+
+
+// 22.	 Write a js program to check whether a number is divisible by 5 and 11 or not.
+
+
+// let num = 12
+
+// if(num % 5 ===0 || num % 11 === 0){
+//     console.log(num, "is divisible by 5 and 11 ");
+// }else{
+//     console.log(num, "is not divisible by 5 and 11 ");
+
+// }
