0a10733375d11723b974edbb56033dd74c356a10
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-04-15 15:55:05 -0700

    Question 39 complete

--- jsPrograms/programs.js
+++ jsPrograms/programs.js
@@ -565,6 +565,55 @@
 
 
 
+
+// 39.	 Write a js program print total number of days in a month using switch case.
+
+// let m = prompt("Enter month number ", "1/2/3/...")
+// let month = parseInt(m)
+
+
+// switch (month) {
+//     case 1:
+//         console.log("31 days");
+//         break;
+//     case 2:
+//         console.log("28/29 days");
+//         break;
+//     case 3:
+//         console.log("31 days");
+//         break;
+//     case 4:
+//         console.log("30 days");
+//         break;
+//     case 5:
+//         console.log("31 days");
+//         break;
+//     case 6:
+//         console.log("30 days");
+//         break;
+//     case 7:
+//         console.log("31 days");
+//         break;
+//     case 8:
+//         console.log("31 days");
+//         break;
+//     case 9:
+//         console.log("30 days");
+//         break;
+//     case 10:
+//         console.log("31 days");
+//         break;
+//     case 11:
+//         console.log("30 days");
+//         break;
+//     case 12:
+//         console.log("31 days");
+//         break;
+//     default:
+//         console.log("Invalid");
+// }
+
+
 // 40.	 Write a js program to check whether an alphabet is vowel or consonant using switch case
 
 // let inp = prompt("Enter an alphabet to check whether an alphabet is vowel or consonant")
